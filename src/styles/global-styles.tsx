88c441fe5c9f272f8b999cfb3976e63ebb225759
oxwazz <muhammad.rahmahalim@gmail.com>
2022-02-01 15:24:49 +0700

    fix: adjust style

--- src/styles/global-styles.tsx
+++ src/styles/global-styles.tsx
@@ -5,6 +5,7 @@ import tw, { css, theme, GlobalStyles as BaseStyles } from 'twin.macro'
 const customStyles = css({
   body: {
     // WebkitTapHighlightColor: theme`colors.purple.500`,
+    WebkitTapHighlightColor: 'transparent',
     ...tw`antialiased`,
   },
 })
